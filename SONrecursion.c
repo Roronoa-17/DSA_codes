44cea8071166f7f73f4aff1078a213dee80f6754
Roronoa-17 <111036213+Roronoa-17@users.noreply.github.com>
2023-08-22 14:39:07 +0530

    sum of n , fact, pow, indirect, nested recursion

---
+++ SONrecursion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int sum (int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    else
+    {
+        return sum(n-1) + n;
+    }
+}
+
+ int Isum(int n)
+ {
+    int s=0;
+    int i;
+    for(i=1;i<=n;i++)
+    {
+        s+=i;
+    }
+    return s;
+ }
+
+
+ int main()
+ {
+    int r=0;
+    r=Isum(5);
+    printf("%d ",r);
+    return 0;
+ }
