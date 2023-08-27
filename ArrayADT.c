3c2906f1e043f254674f8e003d7e610c418c310c
Roronoa-17 <111036213+Roronoa-17@users.noreply.github.com>
2023-08-27 20:59:15 +0530

    set , get , sum ,avg

--- ArrayADT.c
+++ ArrayADT.c
@@ -55,13 +55,146 @@ int Delete(struct Array *arr, int index)
     return 0;
 }
 
+void swap(int *x, int *y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+int LinearSearch(struct Array *arr, int key)
+{
+    for(int i = 0; i<arr->length ; i++)
+    {
+        if(key==arr->A[i])
+        {
+            swap(&arr->A[i], &arr->A[0]);
+            return i;
+        }
+    }
+    return -1;
+}
+
+int BinarySearch(struct Array arr, int key)
+{
+    int l,mid,h;
+    l=0;
+    h=arr.length-1;
+
+    while(l<=h)
+    {
+        mid=(l+h)/2;
+        if(key==arr.A[mid])
+        {
+            return mid;
+        }
+        else if(key<arr.A[mid])
+        {
+            h=mid-1;
+        }
+        else
+        {
+            l=mid+1;
+        }
+    }
+
+    return -1;
+}
+
+int RBinarySearch(int a[], int l, int h, int key)
+{
+    if(l<=h)
+    {
+        int mid = (l+h)/2;
+
+        if(key==a[mid])
+        {
+            return mid;
+        }
+        else if(key<a[mid])
+        {
+            return RBinarySearch(a, l, mid-1, key);
+        }
+        else
+        {
+            return RBinarySearch(a, mid+1, h, key);
+        }
+    }
+
+    return -1;
+}
+
+int Get(struct Array arr, int index)
+{
+    if(index>=0 && index<arr.length)
+    {
+        return arr.A[index];
+    }
+    return -1;
+}
+
+void Set(struct Array *arr, int index, int x)
+{
+    if(index>=0 && index<arr->length)
+    {
+        arr->A[index]=x;
+    }
+}
+
+int Max(struct Array arr)
+{
+    int max = arr.A[0];
+    for(int i=1;i<arr.length; i++)
+    {
+        if(arr.A[i]>max)
+        {
+            max = arr.A[i];
+        }
+    }
+    return max;
+}
+
+int Min(struct Array arr)
+{
+    int min = arr.A[0];
+    for(int i=1;i<arr.length; i++)
+    {
+        if(arr.A[i]<min)
+        {
+            min = arr.A[i];
+        }
+    }
+    return min;
+}
+
+int Sum(struct Array arr)
+{
+    int sum  = 0;
+    for(int i = 0; i<arr.length; i++)
+    {
+        sum+=arr.A[i];
+    }
+    return sum;
+}
+
+float Avg(struct Array arr)
+{
+    return (float)Sum(arr)/arr.length;
+}
+
 int main()
 {
     struct Array arr = {{2,3,4,5,6}, 10, 5};
     
     //Append(&arr, 10);
     //Insert(&arr, 0, 10);
-    printf("%d\n", Delete(&arr,3));
+    //printf("%d\n", RBinarySearch(arr.A,0,arr.length, 9));
+    //printf("%d\n", Get(arr,0));
+    //printf("%d\n", Max(arr));
+    //printf("%d\n", Min(arr));
+    //printf("%d\n", Sum(arr));
+    printf("%f\n", Avg(arr));
     Display(arr);
 
     return 0;
